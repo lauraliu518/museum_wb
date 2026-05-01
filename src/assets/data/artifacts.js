f97acb7f9292107f73a437ca399147771d28b7b4
lauraliu518 <yixuanliu0518@gmail.com>
2026-05-01 14:34:48 -0400

    update data

--- src/assets/data/artifacts.js
+++ src/assets/data/artifacts.js
@@ -12,42 +12,121 @@ export const artifacts = {
         }
     },
     "room3/balcony": { 
-        title: "Monument (Altadena)", 
+        title: {
+            en: "Monument (Altadena)",
+            es: "Monumento (Altadena)"
+        }, 
         artist: "Kelly Akashi", 
         year: "2026", 
-        body: "This artwork is a response to the losses of the Eaton Fire in Altadena, California, in 2025. After the artist's home and studio burned down, only the chimney remained standing. She rebuilt it in glass brick alongside a reconstruction of her home's walkway." 
+        body: {
+        en: "This artwork is a response to the losses of the Eaton Fire in Altadena, California, in 2025. After the artist's home and studio burned down, only the chimney remained standing. She rebuilt it in glass brick alongside a reconstruction of her home's walkway.",
+        es: " Esta obra es una respuesta a las pérdidas de los incendios en Altadena, California, en 2025. La casa y el estudio de la artista se incendiaron y únicamente la chimenea se mantuvo intacta. Akashi reconstruyó la chimenea en ladrillos de vidrio, junto con el corredor de la entrada de su casa."
+        }
     },
     "room2/bigroom/colorful_doll": { 
-        title: "Inflatables", 
+        title: {
+            en: "Inflatables",
+            es: "Inflatables"
+        },
         artist: "Pat Oleszko", 
         year: "1980", 
-        body: "Pat Oleszko has been making inflatable sculptures since the early 1980s, as a way to make large-scale sculptures without relying on the need for constructed, heavy armatures.The artist sews the sculptures in sections, then connects, unfurls, and inflates them on her roof." 
+        body: {
+            en: "Pat Oleszko has been making inflatable sculptures since the early 1980s, as a way to make large-scale sculptures without relying on the need for constructed, heavy armatures.The artist sews the sculptures in sections, then connects, unfurls, and inflates them on her roof." ,
+            es: "Pat Oleszko lleva haciendo esculturas inflables desde los años ochenta, con el fin de hacer esculturas grandes sin tener que usar armazones pesados. La artista cose las esculturas en secciones, las conecta, despliega e infla en su azotea."
+        }
     },
-
-    "room2/bigroom/neon_box": { title: "g3_6", artist: "Unknown", year: "", body: "" },
-    "room2/bigroom/tapestry": { title: "g3_8", artist: "Unknown", year: "", body: "" },
-    "room2/bigroom/media_small": { title: "g3_9", artist: "Unknown", year: "", body: "" },
-  
-    "room2/bigroom/open_media": { title: "g4_1", artist: "Unknown", year: "", body: "" },
-    "room2/bigroom/media_big": { title: "g4_2", artist: "Unknown", year: "", body: "" },
-  
-    "room2/bigroom/trunk": { title: "g5_1", artist: "Unknown", year: "", body: "" },
-  
-    "room2/bigroom/tv_chair": { title: "g6_2b", artist: "Unknown", year: "", body: "" },
-    "room2/bigroom/balloon": { title: "g6_2c", artist: "Unknown", year: "", body: "" },
+    "room2/bigroom/neon_box": { 
+        title: { en: "", es: "" }, 
+        artist: "", 
+        year: "", 
+        body: { 
+            en: "Nile Harris with Dyer Rhoads. End of Days consists of scrolling text on an LED display and performances by Nile Harris and Dyer Rhoads. During the live performances the LED display provides an English translation of the Russian spoken by a man delivered to the galleries and uncrated as a work of art. The work reflects on what it means to be “collected”—a question that is especially relevant for artists working with performance inside of museums, institutions traditionally dedicated to the display and preservation of objects. Dark Brown Birkin Bag is a related work that Harris conceived from a desire to make a Hermès Birkin bag—known for being a collector’s item—out of his own skin.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/tapestry": { 
+        title: { en: "", es: "" }, 
+        artist: "Teresa Baker", 
+        year: "", 
+        body: { 
+            en: "A large green textile wall hanging with curved teal panels and a central red circular patch. Teresa Baker creates paintings by placing yarn, buckskin, and other materials on top of large swaths of synthetic turf. She was drawn to the material because it suggests the land and the space between natural and artificial. She makes the abstract compositions intuitively, drawing inspiration from sources ranging from textured yarn to expansive landscapes.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/media_small": { 
+        title: { en: "", es: "" }, 
+        artist: " Jordan Strafer", 
+        year: "", 
+        body: { 
+            en: "A man with a mustache has a tear on his cheek while someone whispers into his neck. Talk Show is the final film in Jordan Strafer’s Loophole trilogy. The narrative unfolds across two periods of time: the turn of the millennium, shot in color, and the afterlife, shot in black and white. Structured around a talk show, the work examines 1990s celebrity culture, violence against women, and justice as televised spectacle. Its screenplay draws on sources ranging from news footage to self-help media, blending historical fact with fiction.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/open_media": { 
+        title: { en: "", es: "" }, 
+        artist: "Kekahi Wahi", 
+        year: "2020", 
+        body: { 
+            en: "A brightly colored digital rendering depicting colonial arrival overlaid with a woman dressed in modern pink exercise clothes and pop culture iconography. The grassroots film initiative kekahi wahi filmed this satirical resistance training workout on the shore at Kealakekua Bay on the Island of Hawai‘i. It was on this site in 1779 that the high chief Kalani‘ōpu‘u escaped a kidnapping by Captain James Cook, the British Royal Navy officer whose crews became known for spreading diseases to Indigenous people throughout the South Pacific. Today the bay is a major destination for tourists.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/media_big": { 
+        title: { en: "", es: "" }, 
+        artist: "Basel Abbas, Ruanne Abou-Rahme", 
+        year: "2023", 
+        body: { 
+            en: "In this video and sound installation, Ruanne Abou-Rahme and Basel Abbas express what they have described as collective feelings of love and longing felt by many Palestinians in the wake of erasure of land and communities in Palestine. The texts, sounds, and images that appear and then recede throughout Until we became fire and fire us create what the artists call “forms of hauntings.” Punctuated by drawings Abou-Rahme’s father made in Jerusalem in the 1970s and 1980s and other layers of ephemera, the three-channel video weaves together traditional songs with contemporary footage of indigenous plants that resist eradication.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/trunk": { 
+        title: { en: "", es: "" }, 
+        artist: "", 
+        year: "", 
+        body: { 
+            en: " This sculpture is Malcolm Peacock's interpretation of a coastal redwood tree, which he encountered on multiple trips to the Pacific Northwest. Its surface comprises about 3,500 braids, which Peacock made by hand over ten months. As a long-distance runner, Peacock often explores ideas of endurance and performance in his work. The texts on the surface of the sculpture are excerpts from the autobiographies of Frederick Douglass and Malcolm X.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/tv_chair": { 
+        title: { en: "", es: "" }, 
+        artist: "Leo Castañeda", 
+        year: "", 
+        body: { 
+            en: "A surreal green underwater scene with rocks, sea plants, and a large slug-like creature in the foreground. Leo Castañeda adapted 3D modeling techniques and combined painting, drawing, and sculpture to create the immersive video game installation on view here. He incorporated paintings by his grandmother Maria Thereza Negreiros to depict a fantastical landscape of Colombian and Brazilian ecosystems inflected with Latin American Surrealism. As players move through this territory in the game, they use camouflage and electromagnetism to solve environmental puzzles and resolve conflicts, ultimately altering the course of a cataclysmic explosion.", 
+            es: "" 
+        } 
+    },
+    "room2/bigroom/balloon": { 
+        title: { en: "", es: "" }, 
+        artist: "Gabriela Ruiz", 
+        year: "", 
+        body: { 
+            en: "In Homo Machina (Human Machine, a.k.a. Gay Machine), Gabriela Ruiz combines sculpture with video and surveillance footage, constructing a life-size digital console that functions as a self-portrait. The work reflects on the ways technological systems shape visibility, labor, and control, particularly for brown bodies. Drawing on early Windows-era digital aesthetics, Ruiz rejects the sterile minimal look of contemporary technology in favor of bold color, excess, camp, and spectacle.", 
+            es: "" 
+        } 
+    },
     "room2/bigroom/floor_paintings":{
-        title: "Untitled",
+        title: {
+            en: "Untitled",
+            es: "Sin título"
+        },
         artist: "Johanna Unzueta",
-        body: "These drawings are made on paper using natural pigments. These include indigo, which the artist first encountered in Guatemala, and tints from berries found in a friend's garden during the pandemic. To show both sides of the drawings, she took inspiration from tabletop tent signs used in restaurants."
+        body: {
+            en: "These drawings are made on paper using natural pigments. These include indigo, which the artist first encountered in Guatemala, and tints from berries found in a friend's garden during the pandemic. To show both sides of the drawings, she took inspiration from tabletop tent signs used in restaurants.",
+            es: "Estos dibujos están hechos en papel con pigmentos naturales. Estos incluyen índigo, los cuales la artista conoció en Guatemala, y tintes de moras encontradas en el jardín de una amiga durante la pandemia. Para mostrar ambos lados de los dibujos, se inspiró en los letreros de mesa de los restaurantes."
+    
+        },
     },
     "room2/bigroom/wall_old":{
-        title: "Untitled",
-        artist: "Johanna Unzueta",
-        body: "These drawings are made on paper using natural pigments. These include indigo, which the artist first encountered in Guatemala, and tints from berries found in a friend's garden during the pandemic. To show both sides of the drawings, she took inspiration from tabletop tent signs used in restaurants."
+        title: "",
+        artist: "",
+        body: "Kimowan Metchewais took an expansive approach to photography, producing Polaroids that he translated into paintings and collages as well as ephemeral installations on his studio walls. Beginning the 1990s—a time when many Indigenous artists explicitly challenged stereotypes—his works are abstract and poetic in their exploration of Indigenous identity. Emphasizing the tactility and materiality of his self-portraits, landscapes, and other images, Metchewais pursued what he called a “self-made Native imagery.”"
     },
     "room2/bigroom/wall_frames":{
-        title: "Untitled",
-        artist: "Johanna Unzueta",
-        body: "These drawings are made on paper using natural pigments. These include indigo, which the artist first encountered in Guatemala, and tints from berries found in a friend's garden during the pandemic. To show both sides of the drawings, she took inspiration from tabletop tent signs used in restaurants."
+        title: "",
+        artist: "Jasmin Sian",
+        body: "Working with found materials such as deli bags, Jasmin Sian wreathes animals and plants with intricate lace-like motifs that she cuts out with an X-Acto knife. The equally detailed act of painting the living beings at the center of the works—sometimes using brushes with a single bristle—is meant to honor their existence, creating memorials in some cases and protective dwellings in others."
     }
   };
